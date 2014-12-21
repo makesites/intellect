b0bbc1767397032b160386b0e8da0a319120329e
Makis Tracend <makis.tracend@gmail.com>
2014-12-20 23:41:00 -0800

    :zzz: response types

--- config/response.js
+++ config/response.js
@@ -1,7 +1,7 @@
 // Response object
 var response = {
 	format: "text", // defining the media format used for the output - options: text, audio, emoji
-	type: "number",  // giving context to the response - options: number, word, sentence, place, time, preference, abstract
+	type: "number",  // giving context to the response - options: number, word, sentence, place, time, awareness, preference, abstract
 	data: "", // the actuall response data
 	tags: [] // an array of keywords describing the response
 }

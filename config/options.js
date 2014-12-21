5f6bb8774725b826aa72b6557f5e538d69fd6061
Makis Tracend <makis.tracend@gmail.com>
2014-12-20 23:40:33 -0800

    + options: data, interpreter

--- config/options.js
+++ config/options.js
@@ -1,6 +1,8 @@
 // Default init options
 var options = {
-	formats: ["text"] // the input/output types expected to be used - options (TBA): text, audio...
+	formats: ["text"], // the input/output types expected to be used - options (TBA): text, audio...
+	data: null, // provided data, when intellect is in "active" mode
+	interpreter: false, // third-party method to relate tags to responses
 }
 
 module.exports = options;

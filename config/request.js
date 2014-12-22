329ac033b506e1014ce4571fac3ee4e619b1ca6c
Makis Tracend <makis.tracend@gmail.com>
2014-12-21 18:56:20 -0800

    introducing more config options

--- config/request.js
+++ config/request.js
@@ -2,6 +2,7 @@
 var request = {
 	body: "", // input string (or other medium in the future)
 	data: [], // data extracted from the body after analyzing (in text form)
+	types: [], // a container for identified types of input
 	state: [], // the current state to be under consideration
 }
 
--- config/response.js
+++ config/response.js
@@ -3,6 +3,7 @@ var response = {
 	format: "text", // defining the media format used for the output - options: text, audio, emoji
 	type: "number",  // giving context to the response - options: number, word, sentence, place, time, awareness, preference, abstract
 	data: "", // the actuall response data
+	results: {}, // a container for saving the processing results of each individual middleware
 	tags: [] // an array of keywords describing the response
 }
 
